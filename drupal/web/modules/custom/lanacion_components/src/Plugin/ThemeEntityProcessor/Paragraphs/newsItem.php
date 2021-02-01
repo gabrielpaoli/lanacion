60bda01ecfb253c12c447d656fc5bbf6111eaa42
Gabriel Paoli <gabrielpaoli@hotmail.com>
2021-02-01 18:19:17 -0300

    some changes

--- drupal/web/modules/custom/lanacion_components/src/Plugin/ThemeEntityProcessor/Paragraphs/newsItem.php
+++ drupal/web/modules/custom/lanacion_components/src/Plugin/ThemeEntityProcessor/Paragraphs/newsItem.php
@@ -27,7 +27,6 @@ class newsItem extends LaNacionThemeEntityProcessorBase {
     $title = $this->getFieldData($variables['elements']['field_p_title']);
     $show_as = $variables['elements']['#paragraph']->get('field_p_show_as')->value;
     $news = $variables['elements']['#paragraph']->get('field_p_related_news')->referencedEntities();
-    
 
     $variables['data'] = [
       'title' => $title,
@@ -54,6 +53,30 @@ class newsItem extends LaNacionThemeEntityProcessorBase {
 
   }
 
+  public function getImage($node){
+    $data = [];
+
+    $fid = (!empty($node->get('field_image')->getValue()[0]) ? $node->get('field_image')->getValue()[0]['target_id'] : null);
+
+    if(!empty($fid)){
+      $file = File::load($fid);
+      $data = [
+        'uri' => $file->getFileUri(),
+        'alt' => $node->get('field_image')->getValue()[0]['alt']
+      ];
+    }
+
+    return $data;
+  }
+
+  public function getTags($node){
+
+    $term = (!empty($node->get('field_tags')->target_id) ? Term::load($node->get('field_tags')->target_id) : null);
+    $term_name = (!empty($term) ? $term->getName() : null);
+
+    return $term_name;
+  }
+
   public function getNewsData($news){
     $data = [];
     $editor_name = '';
@@ -61,16 +84,15 @@ class newsItem extends LaNacionThemeEntityProcessorBase {
 
     foreach($news as $indNews){
       $url_alias = \Drupal::service('path_alias.manager')->getAliasByPath('/node/'. $indNews->id());
-      $fid = $indNews->get('field_image')->getValue()[0]['target_id'];
 
-      $file = File::load($fid);
-      $uri = $file->getFileUri();
+      $image_data = $this->getImage($indNews);
+      $term_name = $this->getTags($indNews);
 
       $editor = $indNews->get('field_related_editor')->referencedEntities();
-
       $pre_title = $indNews->get('field_pre_title')->value;
 
-      if(isset($editor[0])){
+
+      if(!empty($editor[0])){
         $editor_name = $editor[0]->getTitle();
 
         $fid_editor = $editor[0]->get('field_image')->getValue()[0]['target_id'];
@@ -79,15 +101,15 @@ class newsItem extends LaNacionThemeEntityProcessorBase {
         $uri_editor = $file_editor->getFileUri();
       }
 
-
       $data[] = [
         'title' => $indNews->getTitle(),
         'link' => $url_alias,
-        'image_url' => $uri,
-        'image_alt' => $indNews->get('field_image')->getValue()[0]['alt'],
+        'image_url' => $image_data['uri'],
+        'image_alt' => $image_data['alt'],
         'editor_name' => $editor_name,
         'editor_image' => $uri_editor,
         'pre_title' => $pre_title,
+        'tag' => $term_name
       ];
     }
 
